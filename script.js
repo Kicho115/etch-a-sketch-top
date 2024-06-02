3277ae60da8b49ef5c29b8708b129ae97cdddca2
Kicho115 <oscar.leonardo.ang@gmail.com>
2024-06-01 22:23:16 -0700

    Add a hovering effect that changes the background of the divs of the canvas

--- script.js
+++ script.js
@@ -8,8 +8,13 @@ for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
         const cell = document.createElement('div');
         cell.classList.add('cell');
+        cell.classList.add(`row${i}-col${j}`);
         cell.style.backgroundColor = 'white';
         row.appendChild(cell);
     }
     canvas.appendChild(row);
 }
+
+canvas.addEventListener('mouseover', (e) => {
+    e.target.style.backgroundColor = 'black';
+})
