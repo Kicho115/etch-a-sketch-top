aca7ce00e86f48700daef7f857e70d8305394529
Kicho115 <oscar.leonardo.ang@gmail.com>
2024-06-01 22:42:10 -0700

    Add a function to erase the canvas

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
 const canvas = document.getElementById('canvas');
 const size = 16;
+createCanvas(size);
 
-for (let i = 0; i < size; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    
-    for (let j = 0; j < size; j++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        cell.classList.add(`row${i}-col${j}`);
-        cell.style.backgroundColor = 'white';
-        row.appendChild(cell);
-    }
-    canvas.appendChild(row);
-}
 
 canvas.addEventListener('mouseover', (e) => {
     e.target.style.backgroundColor = 'black';
 })
+
+function createCanvas (size) {
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        
+        for (let j = 0; j < size; j++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.classList.add(`row${i}-col${j}`);
+            cell.style.backgroundColor = 'white';
+            row.appendChild(cell);
+        }
+        canvas.appendChild(row);
+    }
+}
+
+function eraseCanvas () {
+    while (canvas.firstChild) {
+        canvas.removeChild(canvas.lastChild);
+    }
+}
